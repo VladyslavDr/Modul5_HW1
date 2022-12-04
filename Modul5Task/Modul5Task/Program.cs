208ab86b510cbec5fd24e8a1d785c46ee75d1c26
vladislav Drushliak <vladislav.drushliak@gmail.com>
2022-12-04 19:34:36 +0200

    add empty project with git ignore and settings.rulset

---
+++ Modul5Task/Modul5Task/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Modul5Task
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
